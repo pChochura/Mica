3972aab5bd37e16b732a413a8dc688cf8de458ef
Paweł Chochura <pchochura@gmail.com>
2025-08-21 21:36:29 +0200

    Refactor: Extract instruction execution logic into separate functions.
    This change breaks down the large `executeNextInstruction` function into smaller, more manageable functions, each responsible for handling a specific instruction type. This improves code readability and maintainability.

--- mica/src/main/java/com/pointlessapps/granite/mica/runtime/Runtime.kt
+++ mica/src/main/java/com/pointlessapps/granite/mica/runtime/Runtime.kt
@@ -3,7 +3,6 @@ package com.pointlessapps.granite.mica.runtime
 import com.pointlessapps.granite.mica.ast.Root
 import com.pointlessapps.granite.mica.ast.expressions.BooleanLiteralExpression
 import com.pointlessapps.granite.mica.ast.expressions.CharLiteralExpression
-import com.pointlessapps.granite.mica.ast.expressions.Expression
 import com.pointlessapps.granite.mica.ast.expressions.NumberLiteralExpression
 import com.pointlessapps.granite.mica.ast.expressions.StringLiteralExpression
 import com.pointlessapps.granite.mica.ast.expressions.SymbolExpression
@@ -63,152 +62,176 @@ internal class Runtime(private val rootAST: Root) {
         }
     }
 
-    // TODO refactor: simplify
     private suspend fun executeNextInstruction() {
         when (val instruction = instructions[index]) {
             is Instruction.Label -> {} // NOOP
-            Instruction.ReturnFromFunction -> {
-                index = requireNotNull(functionCallStack.removeLastOrNull()) - 1
-                variableScopeStack.removeLastOrNull()
-            }
-
-            Instruction.AcceptInput -> stack.add(StringType.toVariable(onInputCallback()))
-            Instruction.Print -> {
-                val output = requireNotNull(stack.removeLastOrNull()).value as String
-                onOutputCallback(output)
-            }
-
+            Instruction.ReturnFromFunction -> executeReturnFromFunction()
+            Instruction.AcceptInput -> executeAcceptInput()
+            Instruction.Print -> executePrint()
             is Instruction.Jump -> index = instruction.index - 1
-            is Instruction.JumpIf -> {
-                val expressionResult = requireNotNull(stack.removeLastOrNull()).value as Boolean
-                if (expressionResult == instruction.condition) index = instruction.index - 1
-            }
+            is Instruction.JumpIf -> executeJumpIf(instruction)
+            is Instruction.ExecuteExpression -> executeExpression(instruction)
+            is Instruction.ExecuteArrayIndexExpression -> executeArrayIndexExpression()
+            is Instruction.ExecuteArrayLiteralExpression ->
+                executeArrayLiteralExpression(instruction)
 
-            is Instruction.ExecuteExpression ->
-                executeExpression(instruction.expression, variableScope)
+            is Instruction.ExecuteUnaryOperation -> executeUnaryOperation(instruction)
+            is Instruction.ExecuteBinaryOperation -> executeBinaryOperation(instruction)
+            is Instruction.ExecuteFunctionCallExpression ->
+                executeFunctionCallExpression(instruction)
 
-            is Instruction.ExecuteArrayIndexExpression -> stack.add(
-                ArrayIndexExpressionExecutor.execute(
-                    // Reverse order
-                    arrayIndex = requireNotNull(stack.removeLastOrNull()),
-                    arrayValue = requireNotNull(stack.removeLastOrNull()),
-                ),
-            )
-
-            is Instruction.ExecuteArrayLiteralExpression -> stack.add(
-                ArrayLiteralExpressionExecutor.execute(
-                    (1..instruction.elementsCount).map {
-                        requireNotNull(stack.removeLastOrNull())
-                    }.asReversed(),
-                ),
-            )
-
-            is Instruction.ExecuteUnaryOperation -> stack.add(
-                PrefixUnaryOperatorExpressionExecutor.execute(
-                    value = requireNotNull(stack.removeLastOrNull()),
-                    operator = instruction.operator,
-                ),
-            )
-
-            is Instruction.ExecuteBinaryOperation -> stack.add(
-                BinaryOperatorExpressionExecutor.execute(
-                    // Reverse order
-                    rhsValue = requireNotNull(stack.removeLastOrNull()),
-                    lhsValue = requireNotNull(stack.removeLastOrNull()),
-                    operator = instruction.operator,
-                ),
-            )
-
-            is Instruction.ExecuteFunctionCallExpression -> {
-                val arguments = stack.subList(
-                    fromIndex = stack.size - instruction.argumentsCount,
-                    toIndex = stack.size,
-                )
-                val argumentTypes = arguments.map(Variable<*>::type)
-
-                builtinFunctionDeclarations.getMatchingFunctionDeclaration(
-                    name = instruction.functionName,
-                    arguments = argumentTypes,
-                )?.let {
-                    val result = it.execute(
-                        arguments.map { arg ->
-                            arg.type to requireNotNull(arg.value)
-                        },
-                    )
-                    stack.removeAll(arguments)
-                    stack.add(result.first.toVariable(result.second))
-
-                    return
-                }
-
-                functionCallStack.add(index + 1)
-                // Create a scope from the root state
-                variableScopeStack.add(VariableScope.from(variableScopeStack.first()))
-                index = requireNotNull(
-                    functionDeclarations.getMatchingFunctionDeclaration(
-                        name = instruction.functionName,
-                        arguments = argumentTypes,
-                    ),
-                ) - 1
-            }
-
-            is Instruction.AssignVariable -> {
-                val expressionResult = requireNotNull(stack.removeLastOrNull())
-                if (variableScope.get(instruction.variableName) != null) {
-                    variableScope.assignValue(
-                        name = instruction.variableName,
-                        value = requireNotNull(expressionResult.value),
-                    )
-                } else {
-                    variableScope.declare(
-                        name = instruction.variableName,
-                        value = requireNotNull(expressionResult.value),
-                        variableType = expressionResult.type,
-                    )
-                }
-            }
-
-            is Instruction.DeclareVariable -> {
-                val type = requireNotNull(stack.removeLastOrNull()).type
-                val expressionResult = requireNotNull(stack.removeLastOrNull())
-                variableScope.declare(
-                    name = instruction.variableName,
-                    value = requireNotNull(expressionResult.value),
-                    variableType = type,
-                )
-            }
-
-            is Instruction.DeclareFunction -> {
-                val types = (1..instruction.parametersCount).map {
-                    requireNotNull(stack.removeLastOrNull()).type
-                }.asReversed()
-
-                functionDeclarations.getOrPut(
-                    key = instruction.functionName to types.size,
-                    defaultValue = ::mutableMapOf,
-                )[types] = index + 2
-            }
-
+            is Instruction.AssignVariable -> executeAssignVariable(instruction)
+            is Instruction.DeclareVariable -> executeDeclareVariable(instruction)
+            is Instruction.DeclareFunction -> executeDeclareFunction(instruction)
             is Instruction.PushToStack -> stack.add(instruction.value)
-
             Instruction.DeclareScope -> variableScopeStack.add(VariableScope.from(variableScope))
             Instruction.ExitScope -> variableScopeStack.removeLastOrNull()
             Instruction.DuplicateLastStackItem -> stack.add(stack.last())
         }
     }
 
-    private fun executeExpression(expression: Expression, state: VariableScope) {
-        stack.add(
-            when (expression) {
-                is SymbolExpression -> requireNotNull(state.get(expression.token.value))
-                is CharLiteralExpression -> CharType.toVariable(expression.token.value)
-                is StringLiteralExpression -> StringType.toVariable(expression.token.value)
-                is BooleanLiteralExpression -> BoolType.toVariable(expression.token.value.toBooleanStrict())
-                is NumberLiteralExpression -> when (expression.token.type) {
-                    Token.NumberLiteral.Type.Real, Token.NumberLiteral.Type.Exponent ->
-                        RealType.toVariable(expression.token.value.toRealNumber())
+    private fun executeDeclareFunction(instruction: Instruction.DeclareFunction) {
+        val types = (1..instruction.parametersCount).map {
+            requireNotNull(stack.removeLastOrNull()).type
+        }.asReversed()
 
-                    else -> IntType.toVariable(expression.token.value.toIntNumber())
+        functionDeclarations.getOrPut(
+            key = instruction.functionName to types.size,
+            defaultValue = ::mutableMapOf,
+        )[types] = index + 2
+    }
+
+    private fun executeDeclareVariable(instruction: Instruction.DeclareVariable) {
+        val type = requireNotNull(stack.removeLastOrNull()).type
+        val expressionResult = requireNotNull(stack.removeLastOrNull())
+        variableScope.declare(
+            name = instruction.variableName,
+            value = requireNotNull(expressionResult.value),
+            variableType = type,
+        )
+    }
+
+    private fun executeAssignVariable(instruction: Instruction.AssignVariable) {
+        val expressionResult = requireNotNull(stack.removeLastOrNull())
+        if (variableScope.get(instruction.variableName) != null) {
+            variableScope.assignValue(
+                name = instruction.variableName,
+                value = requireNotNull(expressionResult.value),
+            )
+        } else {
+            variableScope.declare(
+                name = instruction.variableName,
+                value = requireNotNull(expressionResult.value),
+                variableType = expressionResult.type,
+            )
+        }
+    }
+
+    private fun executeFunctionCallExpression(instruction: Instruction.ExecuteFunctionCallExpression) {
+        val arguments = stack.subList(
+            fromIndex = stack.size - instruction.argumentsCount,
+            toIndex = stack.size,
+        )
+        val argumentTypes = arguments.map(Variable<*>::type)
+
+        builtinFunctionDeclarations.getMatchingFunctionDeclaration(
+            name = instruction.functionName,
+            arguments = argumentTypes,
+        )?.let {
+            val result = it.execute(
+                arguments.map { arg ->
+                    arg.type to requireNotNull(arg.value)
+                },
+            )
+            stack.removeAll(arguments)
+            stack.add(result.first.toVariable(result.second))
+
+            return
+        }
+
+        functionCallStack.add(index + 1)
+        // Create a scope from the root state
+        variableScopeStack.add(VariableScope.from(variableScopeStack.first()))
+        index = requireNotNull(
+            functionDeclarations.getMatchingFunctionDeclaration(
+                name = instruction.functionName,
+                arguments = argumentTypes,
+            ),
+        ) - 1
+    }
+
+    private fun executeBinaryOperation(instruction: Instruction.ExecuteBinaryOperation) {
+        stack.add(
+            BinaryOperatorExpressionExecutor.execute(
+                // Reverse order
+                rhsValue = requireNotNull(stack.removeLastOrNull()),
+                lhsValue = requireNotNull(stack.removeLastOrNull()),
+                operator = instruction.operator,
+            ),
+        )
+    }
+
+    private fun executeUnaryOperation(instruction: Instruction.ExecuteUnaryOperation) {
+        stack.add(
+            PrefixUnaryOperatorExpressionExecutor.execute(
+                value = requireNotNull(stack.removeLastOrNull()),
+                operator = instruction.operator,
+            ),
+        )
+    }
+
+    private fun executeArrayLiteralExpression(instruction: Instruction.ExecuteArrayLiteralExpression) {
+        stack.add(
+            ArrayLiteralExpressionExecutor.execute(
+                (1..instruction.elementsCount).map {
+                    requireNotNull(stack.removeLastOrNull())
+                }.asReversed(),
+            ),
+        )
+    }
+
+    private fun executeArrayIndexExpression() {
+        stack.add(
+            ArrayIndexExpressionExecutor.execute(
+                // Reverse order
+                arrayIndex = requireNotNull(stack.removeLastOrNull()),
+                arrayValue = requireNotNull(stack.removeLastOrNull()),
+            ),
+        )
+    }
+
+    private fun executeJumpIf(instruction: Instruction.JumpIf) {
+        val variable = requireNotNull(stack.removeLastOrNull())
+        val expressionResult = variable.type.valueAsSupertype<BoolType>(variable.value) as Boolean
+        if (expressionResult == instruction.condition) index = instruction.index - 1
+    }
+
+    private fun executePrint() {
+        val variable = requireNotNull(stack.removeLastOrNull())
+        onOutputCallback(variable.type.valueAsSupertype<StringType>(variable.value) as String)
+    }
+
+    private suspend fun executeAcceptInput() {
+        stack.add(StringType.toVariable(onInputCallback()))
+    }
+
+    private fun executeReturnFromFunction() {
+        index = requireNotNull(functionCallStack.removeLastOrNull()) - 1
+        variableScopeStack.removeLastOrNull()
+    }
+
+    private fun executeExpression(instruction: Instruction.ExecuteExpression) {
+        stack.add(
+            when (instruction.expression) {
+                is SymbolExpression -> requireNotNull(variableScope.get(instruction.expression.token.value))
+                is CharLiteralExpression -> CharType.toVariable(instruction.expression.token.value)
+                is StringLiteralExpression -> StringType.toVariable(instruction.expression.token.value)
+                is BooleanLiteralExpression -> BoolType.toVariable(instruction.expression.token.value.toBooleanStrict())
+                is NumberLiteralExpression -> when (instruction.expression.token.type) {
+                    Token.NumberLiteral.Type.Real, Token.NumberLiteral.Type.Exponent ->
+                        RealType.toVariable(instruction.expression.token.value.toRealNumber())
+
+                    else -> IntType.toVariable(instruction.expression.token.value.toIntNumber())
                 }
 
                 else -> throw IllegalStateException("Such expression should not be evaluated")
