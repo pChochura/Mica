839cc058ad0398403c54e5ba497cf868df4d2788
Paweł Chochura <pchochura@gmail.com>
2025-10-02 22:05:02 +0200

    Feat: Add compiler context and instructions
    Introduces the `CompilerContext` to manage scope, types, and functions during compilation. Also adds the sealed interface `CompilerInstruction` and its various implementations for bytecode generation.

---
+++ mica/src/main/java/com/pointlessapps/granite/mica/compiler/model/CompilerContext.kt
@@ -0,0 +1,120 @@
+package com.pointlessapps.granite.mica.compiler.model
+
+import com.pointlessapps.granite.mica.ast.expressions.Expression
+import com.pointlessapps.granite.mica.builtins.builtinFunctions
+import com.pointlessapps.granite.mica.builtins.builtinTypeProperties
+import com.pointlessapps.granite.mica.builtins.functions.BuiltinFunctionDeclaration
+import com.pointlessapps.granite.mica.linter.model.FunctionOverload
+import com.pointlessapps.granite.mica.linter.model.Scope
+import com.pointlessapps.granite.mica.linter.model.ScopeType
+import com.pointlessapps.granite.mica.linter.model.TypeProperty
+import com.pointlessapps.granite.mica.linter.resolver.TypeResolver
+import com.pointlessapps.granite.mica.model.Location
+import com.pointlessapps.granite.mica.model.Token
+import com.pointlessapps.granite.mica.model.Type
+
+internal class CompilerContext {
+
+    private val emptyToken = Token.Invalid(Location.EMPTY, "")
+    private val loopLabelStack: MutableList<String> = mutableListOf()
+
+    private val globalScope: Scope = Scope(ScopeType.Root, null).apply {
+        addFunctions(
+            builtinFunctions.groupingBy(BuiltinFunctionDeclaration::name)
+                .aggregate { _, acc: MutableMap<List<FunctionOverload.Parameter>, FunctionOverload>?, element, first ->
+                    val overload = FunctionOverload(
+                        typeParameterConstraint = element.typeParameterConstraint,
+                        parameters = element.parameters,
+                        getReturnType = element.getReturnType,
+                        accessType = FunctionOverload.AccessType.GLOBAL_AND_MEMBER,
+                        isBuiltin = true,
+                    )
+                    if (first) {
+                        mutableMapOf(element.parameters to overload)
+                    } else {
+                        requireNotNull(acc).apply { put(element.parameters, overload) }
+                    }
+                }.toMutableMap(),
+        )
+        addTypeProperties(
+            builtinTypeProperties.groupingBy { it.receiverType }
+                .aggregate { _, acc: MutableMap<String, TypeProperty>?, element, first ->
+                    val typeProperty = TypeProperty(
+                        name = element.name,
+                        receiverType = element.receiverType,
+                        returnType = element.returnType,
+                        isBuiltin = true,
+                    )
+                    if (first) {
+                        mutableMapOf(element.name to typeProperty)
+                    } else {
+                        requireNotNull(acc).apply { put(element.name, typeProperty) }
+                    }
+                },
+        )
+    }
+    private val typeResolver = TypeResolver(globalScope)
+
+    fun removeLastLoopLabel(): String? = loopLabelStack.removeLastOrNull()
+    fun addLoopLabel(label: String) {
+        loopLabelStack.add(label)
+    }
+
+    fun containsVariable(name: String) = globalScope.containsVariable(name)
+    fun declareVariable(
+        name: String,
+        type: Type,
+    ) = globalScope.declareVariable(
+        startingToken = emptyToken,
+        name = name,
+        type = type,
+        overrideExisting = true,
+    )
+
+    fun declareFunction(
+        name: String,
+        typeParameterConstraint: Type?,
+        parameters: List<FunctionOverload.Parameter>,
+        returnType: Type,
+    ) = globalScope.declareFunction(
+        startingToken = emptyToken,
+        name = name,
+        typeParameterConstraint = typeParameterConstraint,
+        parameters = parameters,
+        returnType = returnType,
+        accessType = FunctionOverload.AccessType.GLOBAL_AND_MEMBER,
+        overrideExisting = true,
+    )
+
+    fun declareType(
+        name: String,
+        parentType: Type?,
+        properties: Map<String, Type>,
+    ): Type {
+        globalScope.declareType(
+            startingToken = emptyToken,
+            name = name,
+            parentType = parentType,
+            properties = properties,
+            overrideExisting = true,
+        )
+
+        return requireNotNull(
+            value = globalScope.getType(name),
+            lazyMessage = { "No type found for $name" },
+        )
+    }
+
+    fun getMatchingFunctionDeclaration(name: String, argumentTypes: List<Type>) = requireNotNull(
+        value = globalScope.getMatchingFunctionDeclaration(name, argumentTypes),
+        lazyMessage = { "No matching function found for $name" },
+    )
+
+    fun getMatchingTypeProperty(type: Type, property: String) = requireNotNull(
+        value = globalScope.getMatchingTypeProperty(type, property),
+        lazyMessage = { "No matching type property found for $type.$property" },
+    )
+
+    fun resolveExpressionType(expression: Expression): Type =
+        typeResolver.resolveExpressionType(expression)
+}
---
+++ mica/src/main/java/com/pointlessapps/granite/mica/compiler/model/CompilerInstruction.kt
@@ -0,0 +1,72 @@
+package com.pointlessapps.granite.mica.compiler.model
+
+import com.pointlessapps.granite.mica.model.Type
+import kotlin.properties.Delegates
+
+internal sealed interface CompilerInstruction
+
+internal sealed class DestinationInstruction(val label: String) : CompilerInstruction {
+    var index by Delegates.notNull<Int>()
+}
+
+internal class Label(val label: String) : CompilerInstruction
+internal class Jump(label: String) : DestinationInstruction(label)
+internal class JumpIfTrue(label: String) : DestinationInstruction(label)
+internal class JumpIfFalse(label: String) : DestinationInstruction(label)
+
+internal class Call(label: String) : DestinationInstruction(label)
+internal class CallBuiltin(val signature: String) : CompilerInstruction
+internal object Return : CompilerInstruction
+
+internal class Load(val identifier: String) : CompilerInstruction
+internal class Store(val identifier: String) : CompilerInstruction
+internal class StoreLocal(val identifier: String) : CompilerInstruction
+internal object StoreAtIndex : CompilerInstruction
+
+internal class CastAs(val type: Type) : CompilerInstruction
+
+internal class Push(val value: Any?) : CompilerInstruction
+internal object Pop : CompilerInstruction
+internal object Dup : CompilerInstruction
+internal object Rot2 : CompilerInstruction
+internal object Rot3 : CompilerInstruction
+
+internal class NewArray(val size: Int) : CompilerInstruction
+internal class NewSet(val size: Int) : CompilerInstruction
+internal class NewMap(val size: Int) : CompilerInstruction
+internal class NewObject(
+    val name: String,
+    val parentType: Type?,
+    val propertiesCount: Int,
+) : CompilerInstruction
+
+internal object AccessIndex : CompilerInstruction
+internal class AccessBuiltinProperty(val type: Type) : CompilerInstruction
+
+internal sealed interface UnaryInstruction : CompilerInstruction
+internal object UnaryAdd : UnaryInstruction
+internal object UnarySubtract : UnaryInstruction
+internal object UnaryNot : UnaryInstruction
+
+internal sealed interface BinaryInstruction : CompilerInstruction
+internal object BinaryAdd : BinaryInstruction
+internal object BinarySubtract : BinaryInstruction
+internal object BinaryMultiply : BinaryInstruction
+internal object BinaryDivide : BinaryInstruction
+internal object BinaryModulo : BinaryInstruction
+internal object BinaryExponent : BinaryInstruction
+
+internal object BinaryOr : BinaryInstruction
+internal object BinaryAnd : BinaryInstruction
+
+internal object BinaryEquals : BinaryInstruction
+internal object BinaryNotEquals : BinaryInstruction
+internal object BinaryGraterThan : BinaryInstruction
+internal object BinaryLessThan : BinaryInstruction
+internal object BinaryGraterThanOrEquals : BinaryInstruction
+internal object BinaryLessThanOrEquals : BinaryInstruction
+
+internal object BinaryRange : BinaryInstruction
+
+internal object Print : CompilerInstruction
+internal object ReadInput : CompilerInstruction
